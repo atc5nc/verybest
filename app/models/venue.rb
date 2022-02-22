3f25aaa26e32014782ffe02babb69fc96a53ea18
anonymous anonymous <atc5nc@virginia.edu>
2022-02-22 22:30:32 +0000

    Indirect association between users and venues

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -8,6 +8,10 @@ class Venue < ApplicationRecord
 
   # Indirect associations
 
+  has_many   :users,
+             :through => :dishes,
+             :source => :users
+
   # Validations
 
   # Scopes
